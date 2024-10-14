323475a18544cda35f476ca59d59aaf7b5451a13
almuheetushihab <almuheetu@gmail.com>
2024-10-14 14:58:53 +0600

    apps title add in login screen

--- app/src/main/java/com/example/loginscreen/LoginScreen.kt
+++ app/src/main/java/com/example/loginscreen/LoginScreen.kt
@@ -2,9 +2,12 @@ package com.example.loginscreen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,8 +17,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.loginscreen.ui.theme.LoginScreenTheme
 
 @Composable
@@ -38,6 +44,15 @@ fun LoginScreen() {
             modifier = Modifier
                 .size(120.dp)
                 .padding(top = 24.dp)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Awesome\nShop",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
         )
     }
 }
